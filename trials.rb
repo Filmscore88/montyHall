2239476ed2331153bf2d71f30f6b2bf92b49dc17
filmscore88 <cmtmorales24@gmail.com>
2020-02-05 15:24:33 -0600

    imported game outcome photo

--- trials.rb
+++ trials.rb
@@ -1,4 +1,4 @@
-def game(switch)
+def montyGame(switch)
   doors = [0,1,2]
   car = rand(3)
   pickedDoor = rand(3)
@@ -19,10 +19,10 @@ stayed = 0
 switched = 0
 
 trials.times do |trial|
-  stayed   += 1 if game(false)
-  switched += 1 if game(true)
+  stayed   += 1 if montyGame(false)
+  switched += 1 if montyGame(true)
 end
 
-puts "Win frequencies in Monty Hall when"
+puts "Monty Hall Game Outcome Staying Vs Switching"
 puts "    staying: #{100.0 * stayed / trials}%"
 puts "  switching: #{100.0 * switched/ trials}%"
